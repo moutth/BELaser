4e8833b14e728a93bd05db84d76656183269ef56
Riffard Matthias <riffard@etud.insa-toulouse.fr>
2021-05-27 10:43:32 +0200

    DFT corrigée, config des interruptions

--- PjtKEIL_StepDFT/Src/principal.c
+++ PjtKEIL_StepDFT/Src/principal.c
@@ -5,28 +5,23 @@
 int DFT_ModuleAuCarre( short int * Signal64ech, char k);
 extern short int LeSignal;
 
-int tab[64];
+short int dma_buf[64];
 
 int main(void)
 {
 
-// ===========================================================================
-// ============= INIT PERIPH (faites qu'une seule fois)  =====================
-// ===========================================================================
+	// ===========================================================================
+	// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+	// ===========================================================================
 
-// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
-CLOCK_Configure();
-
-for (int i = 0; i<64; i++){
-	tab[i] = DFT_ModuleAuCarre(&LeSignal, i);
-}
-
-
-//============================================================================	
+	// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+	CLOCK_Configure();
 	
+
+	//============================================================================	
 	
-while	(1)
-	{
+		
+	while	(1) {
 	}
 }
 
